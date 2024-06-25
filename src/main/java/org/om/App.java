a94728276f4b49e7f441a2fea47cdd28606e9efe
DemyDeni <demydeni@gmail.com>
2024-06-25 17:24:38 +0200

    Add necessary functions to Manager

--- src/main/java/org/om/App.java
+++ src/main/java/org/om/App.java
@@ -1,21 +1,15 @@
 package org.om;
 
-import org.om.graph.*;
+import org.om.ga.Genotype;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
         Manager manager = new Manager();
-
-        Item item1 = new Item(1, 10, 0.5);
-        Item item2 = item1.clone();
+        Genotype best = manager.simulateOneByOne(10, 100, 100);
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
--- src/main/java/org/om/Manager.java
+++ src/main/java/org/om/Manager.java
@@ -2,6 +2,8 @@ package org.om;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.om.ga.Genotype;
+import org.om.ga.Population;
 import org.om.graph.Graph;
 import org.om.graph.GraphConfig;
 import org.om.graph.Item;
@@ -9,14 +11,12 @@ import org.om.graph.Person;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Getter
 @Setter
 public class Manager {
-    private Graph graph;
-
-    // list of choices for each person for each day
-    private ArrayList<HashMap<Person, HashMap<Item, Integer>>> personChoices;
+    private Graph startingGraph;
 
     public Manager() {
         GraphConfig config = GraphConfig.builder()
@@ -32,30 +32,73 @@ public class Manager {
                 .distributorsNum(2)
                 .manufacturerItemsPerDay(60)
                 .build();
-        graph = new Graph(config);
+        startingGraph = new Graph(config);
     }
 
-    public void initializePersonChoices(int days) {
-        personChoices = new ArrayList<>(days);
+    public Genotype simulateOneByOne(Integer days, Integer populationNum, Integer generationsNum) {
+        // list of choices for each person for each day
+        ArrayList<HashMap<Person, HashMap<Item, Integer>>> personChoices = initializePersonChoices(days);
+
+        // graphs for population
+        List<Graph> graphs = new ArrayList<>();
+        for (int i = 0; i < populationNum; i++) {
+            graphs.add(startingGraph.clone());
+        }
+        // genotypes fer each population
+        Population population = getRandomPopulation(startingGraph, populationNum, 10, 0.15d);
+        // simulate day-by-day
+        for (int d = 0; d < days; d++) {
+            for (int g = 0; g < population.getSize(); g++) {
+                simulateDayForGenotype(generationsNum, graphs.get(g), population.getGenotypes().get(g), personChoices.get(d));
+            }
+
+            Population newPopulation = new Population();
+            // leave 1/4 of original genotypes
+            newPopulation.getGenotypes().addAll(population.getRandomGenotypes(population.getSize() / 4));
+
+            // add 1/4 genotypes after crossover
+            newPopulation.getGenotypes().addAll(population.getRandomGenotypesWithCrossover(population.getSize() / 4));
+
+            // add 2/4 genotypes after crossover and mutation
+            newPopulation.getGenotypes().addAll(population.getRandomGenotypesWithMutation(population.getSize() / 4));
+
+            population = newPopulation;
+        }
+
+        return null;
+    }
+
+
+    private ArrayList<HashMap<Person, HashMap<Item, Integer>>> initializePersonChoices(Integer days) {
+        ArrayList<HashMap<Person, HashMap<Item, Integer>>> personChoices = new ArrayList<>(days);
         for (int i = 0; i < days; i++) {
-            HashMap<Person, HashMap<Item, Integer>> personChoicesForDay = new HashMap<>(graph.getPersons().size());
-            for (Person person : graph.getPersons()) {
-                personChoicesForDay.put(person, person.generateNewOrders(graph.getItems()));
+            HashMap<Person, HashMap<Item, Integer>> personChoicesForDay = new HashMap<>(startingGraph.getPersons().size());
+            for (Person person : startingGraph.getPersons()) {
+                personChoicesForDay.put(person, person.generateNewOrders(startingGraph.getItems()));
             }
             personChoices.add(personChoicesForDay);
         }
+        return personChoices;
     }
 
-
-    public void nextDay() {
-//        manufacturer.generateNewItems();
-//        persons.forEach(p -> p.generateNewOrders(items));
-
-//        List<Task> tasks = ga.getTasks();
-//        updateGraph(tasks);
+    private Population getRandomPopulation(Graph graph, Integer population, Integer moveItemNum, Double moveItemChance) {
+        Population newPopulation = new Population();
+        for (int i = 0; i < population; i++) {
+            newPopulation.getGenotypes().add(Genotype.generateRandomGenotype(graph, 10, 0.15d));
+        }
+        return newPopulation;
     }
 
-    public void simulate(int days) {
+    private void simulateDayForGenotype(Integer generations, Graph graph, Genotype genotype, HashMap<Person, HashMap<Item, Integer>> personChoices) {
+        for (int i = 0; i < generations; i++) {
+            // generate new items on manufacture
+            graph.getManufacturer().generateNewItems();
 
+            // set predefined choices for each person
+            graph.applyPersonChoices(personChoices);
+
+            // apply genotype and calculate fitness
+            graph.applyGenotype(genotype);
+        }
     }
 }
